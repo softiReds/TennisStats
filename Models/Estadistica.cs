3a35e834c612d255081769e1ec2585c15e4745d4
Santiago Rojas <santirojastr12@gmail.com>
2024-01-16 14:39:20 -0500

    Creación de modelo Estadistica

---
+++ Models/Estadistica.cs
@@ -0,0 +1,11 @@
+﻿namespace TennisStats.Models
+{
+    public class Estadistica
+    {
+        public int EstadisticaId { get; set; }
+        public Usuario UsuarioId { get; set; }
+        public int Saques {  get; set; }
+        public int Dobles {  get; set; }
+        public int Errores { get; set; }
+    }
+}
