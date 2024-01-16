9bb2408abef583dc1f9a06453e7cdf21d48515ea
Santiago Rojas <santirojastr12@gmail.com>
2024-01-16 14:41:55 -0500

    Creación de modelo Pistas

---
+++ Models/Pistas.cs
@@ -0,0 +1,10 @@
+﻿namespace TennisStats.Models
+{
+    public class Pistas
+    {
+        public int PistaId { get; set; }
+        public string Nombre { get; set; }
+        public string HoraApertura { get; set; }
+        public string HoraCierre { get; set; }
+    }
+}
