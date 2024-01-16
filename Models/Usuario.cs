f9a21da1984176e01f444f91b59a1680d3ec4da7
Santiago Rojas <santirojastr12@gmail.com>
2024-01-16 14:19:29 -0500

    Creación de modelo Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace TennisStats.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public int Edad {  get; set; }
+        public Genero GeneroId { get; set; }
+        public string CorreoElectronico {  get; set; }
+        public string Contraseña {  get; set; }
+    }
+}
