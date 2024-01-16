0a2c83a696b1308ecb1c833a61facd85ce7d5d54
Santiago Rojas <santirojastr12@gmail.com>
2024-01-16 14:33:50 -0500

    Creación de modelo Partido

---
+++ Models/Partido.cs
@@ -0,0 +1,17 @@
+﻿namespace TennisStats.Models
+{
+    public class Partido
+    {
+        public int PartidoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public Usuario UsuarioGanadorId { get; set; }
+        public int SetsJugados {  get; set; }
+        public Usuario Usuario1Id { get; set; }
+        public Usuario Usuario2Id { get; set; }
+
+        public virtual Usuario Ganador { get; set; }
+        public virtual Usuario Usuario1 { get; set; }
+        public virtual Usuario Usuario2 { get; set; }
+
+    }
+}
